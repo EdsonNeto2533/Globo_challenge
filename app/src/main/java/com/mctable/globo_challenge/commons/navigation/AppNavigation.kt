9d4100a56a13a9aef09c7c14c46362ace8d3dbcd
Edson <edsonneto2533@gmail.com>
2022-06-18 16:36:33 -0300

    homeScreen viewModel injection created

--- app/src/main/java/com/mctable/globo_challenge/commons/navigation/AppNavigation.kt
+++ app/src/main/java/com/mctable/globo_challenge/commons/navigation/AppNavigation.kt
@@ -1,16 +1,18 @@
 package com.mctable.globo_challenge.commons.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.mctable.globo_challenge.home.ui.HomeScreen
+import com.mctable.globo_challenge.home.ui.viewmodel.HomeViewModel
+import com.mctable.globo_challenge.home.ui.views.HomeScreen
 import com.mctable.globo_challenge.splash.ui.components.SplashScreen
 
 @Composable
 fun AppNavGraph() {
+
 
     val navController: NavHostController = rememberNavController()
 
@@ -24,7 +26,7 @@ fun AppNavGraph() {
         }
 
         composable(route = NavigationRoutes.Home.route) {
-            HomeScreen()
+            HomeScreen(viewModel = hiltViewModel())
         }
     }
 }
--- app/src/main/java/com/mctable/globo_challenge/home/ui/views/HomeScreen.kt
+++ app/src/main/java/com/mctable/globo_challenge/home/ui/views/HomeScreen.kt
@@ -1,9 +1,16 @@
-package com.mctable.globo_challenge.home.ui
+package com.mctable.globo_challenge.home.ui.views
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.mctable.globo_challenge.home.ui.viewmodel.HomeViewModel
+import dagger.hilt.android.internal.lifecycle.HiltViewModelFactory
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: HomeViewModel) {
+    LaunchedEffect(true) {
+
+    }
     Text(text = "Aloooooo")
 }
